e05f5259367fee6649ce62c52a9892e197ce2b21
bt-contest <bt.contest.code@gmail.com>
2016-03-28 14:58:20 +0400

    Update install.sh

--- install.sh
+++ install.sh
@@ -4,5 +4,7 @@ cd contest-server
 cp config-templates config -r
 mkdir log
 composer install
+echo "$(tput setaf 0)$(tput setab 2)Complete"
+echo "Configure phinx.yml properly then run:"
+echo "vendor/robmorgan/phinx/bin/phinx migrate$(tput sgr 0)"
 
-echo "Complete"
